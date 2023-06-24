2f79335df7d9dba1d2206f03b3cf7be28a26ec72
Mirko Zlatunic <110959892+mirkozlatunic@users.noreply.github.com>
2023-06-24 09:23:00 -0700

    First correction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
-import { openPopup, closePopup, closeModalWithEsc } from "../utils/utils.js";
 import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 import "../pages/index.css";
 
@@ -146,15 +147,18 @@ editFormValidator.enableValidation();
 
 export { previewImage, previewFooter, previewImageModal };
 
-const defaultDestinationSection = new Section(
+const cardSection = new Section(
   {
-    items: initialCards,
-    renderer: renderCard,
+    data: [],
+    render: renderCard,
   },
-  destinations
+  cardListSelector
 );
 
-defaultDestinationSection.renderItems();
+function renderCard(cardData) {
+  const cardImage = createCard(cardData);
+  cardSection.prependItem(cardImage);
+}
 
 const userInfo = new UserInfo(profileNameSelector, profileProfessionSelector);
 
